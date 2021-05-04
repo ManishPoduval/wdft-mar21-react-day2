8e426c284f2c2ef6482bd525f3eade978e5591f3
ManishPoduval <manishpoduval@hotmail.com>
2021-05-04 19:36:06 +0530

    Refactored Student to hooks

--- src/components/Students.js
+++ src/components/Students.js
@@ -1,23 +1,17 @@
-import React, { Component } from 'react'
+import React, {useState} from 'react'
 import data from '../students.json'
 import StudentDetail from './StudentDetail'
 
-class Students extends Component {
+export default function Students() {
 
-    // create a new state, 
-    // storing all json data in the students state
-    state = {
-        students: data
-    }
+    const [students, updateStudents] = useState(data)
 
-    handleSort = () => {
+    const handleSort = () => {
         // sort the students here 
         // re render the component
         // use the sort()
-        // use the setState({}, callback)
 
         // when you use array methods that mutate/change the original array always deep clone it
-        const {students} = this.state
         let clonedStudent = JSON.parse(JSON.stringify(students))
 
         clonedStudent.sort((a, b) => {
@@ -34,13 +28,12 @@ class Students extends Component {
 
         
         //update the original state after sort is done
-        this.setState({
-            students: clonedStudent
-        })
+        // calls the hooks handler declared on line 6
+        updateStudents(clonedStudent)
 
     }
 
-    handleAdd = () => {
+    const handleAdd = () => {
         // grab a random value from data
         // add it to students
         let randomIndex = Math.floor(Math.random() * data.length)
@@ -48,35 +41,30 @@ class Students extends Component {
 
         // using the spread operator to grab all elemtents of students one by one and store it in the new array
         // [...this.state.students]
-        this.setState({
-            students: [elem, ...this.state.students]
-        })
+
+        // calls the hooks handler declared on line 6
+        updateStudents( [elem, ...students])
 
     }
 
-    handleDelete = (someId) => {
+    const handleDelete = (someId) => {
         console.log('Delete works', someId)
         
-        const {students} = this.state
-
         // keep all the elements that do not match the id
         let filteredStudents = students.filter((singleKid) => {
             // return is a condition on how to filter elements
             return singleKid.id !== someId
         })
 
-        this.setState({
-            students: filteredStudents
-        })
-
+        // calls the hooks handler declared on line 6
+        updateStudents(filteredStudents)
     }
 
-    render() {
-        const {students} = this.state
-        return (
-            <>
-            <button onClick={this.handleSort}>Sort</button>
-            <button onClick={this.handleAdd}>Add</button>
+
+    return (
+        <>
+            <button onClick={handleSort}>Sort</button>
+            <button onClick={handleAdd}>Add</button>
                 { 
                 // key is used internally by React to uniquely identify similar elements/components
                 // can never be accessed in props
@@ -84,14 +72,12 @@ class Students extends Component {
                         return <StudentDetail 
                                 key={index}    
                                 student={singleStudent}
-                                onDelete={this.handleDelete}
+                                onDelete={handleDelete}
                                 />
                     })
                 }
             </>
-        )
-    }
+    )
 }
 
 
-export default Students
